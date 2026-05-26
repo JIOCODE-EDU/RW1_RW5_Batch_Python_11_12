c825ef68059c5bc43906832680509d0c6a64c08d
jiocode-edu <ifmdac80@gmail.com>
2026-05-26 15:07:39 +0530

    26-05-2026

--- Repeat/program/logicbox.py
+++ Repeat/program/logicbox.py
@@ -1,7 +1,8 @@
 # Main program logic with loop and controlled statements.
-'''
+
 while True:
     print("Welcome to the pattern Generator and Number analyzer!!")
+    
     print("1. Generate a Pattern")
     print("2. Analyze a Range of Numbers")
     print("3. Exit")
@@ -18,31 +19,56 @@ while True:
 
             pattern_choice = input("Enter your choice:")
 
-            if pattern_choice in ["1" , "2" , "3"]:
-                while True:
-                    rows = input("Enter the number of rows for the pattern:")
+            rows = int(input("Enter the number of rows for the pattern:"))
+            
+            print("\n Parrern:")
 
-                    if rows.isdigit():
-                        
-                        rows = int(rows)
+            if pattern_choice == "1":
 
-                        if rows < 0:
-                            print("Number of row must be positive")
+                #  Right-angle Triangle
 
-                    else:
-                        print("Invalid Input. Please enter an integer.")
+                    for i in range(1 , rows + 1):
+                        print("*" * i)
 
-                print("\n Parrern:")
+            elif pattern_choice == "2":
 
-                if pattern_choice == "1":
+                # Pyramid
+                    for i in range(1 , rows + 1):
+                        print(" " * (rows-i) , end="")
 
-                    #  Right-angle Triangle
-'''
-for i in range(1 , 6):
-    for j in range(i + 2):
-        print(j , end="")
-    print()
+                        print("*" * (2 * i - 1))
 
+            elif pattern_choice == "3":
+
+                # Left-angle Triangle
+
+                for i in range(1 , rows + 1):
+                    print("  " * (rows - i) + "*" * i)
+                    
+    elif choice == "2":
+
+            start = int(input("Enter start number: "))
+            end = int(input("Enter end number:"))
+
+            total = 0
+
+            print()
+
+            for num in range(start , end + 1):
+                if num % 2 == 0:
+                    print(num , "is even")
+                else:
+                    print(num , "is Odd")
+
+                total = total + num
+
+            print("\n Sum = " , total)
+
+    elif choice == "3":
+        print("Program end.")
+
+    else:
+        print("Invalid Choice")
                         
                     
 
